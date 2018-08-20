66565a8eacacf75312c1ab109c2e53568692d2b1
d9sskssssjss <42514461+wonga20082018@users.noreply.github.com>
2018-08-20 14:19:17 +0200

    Add files via upload

---
+++ app1/UnitTestCoreSender/TestCoreSender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleCoreSender;
+
+namespace UnitTestCoreSender
+{
+    [TestClass]
+    public class TestCoreSender
+    {
+        [TestMethod]
+        public void CheckPrompt()
+        {
+            string expected = "Enter name.";
+            Assert.AreEqual(expected, Prompt.DisplayEnterMsg());
+        }
+
+         [TestMethod]
+        public void DisplaySentMessage()
+        {
+            string msg = "Cat";
+            string expected = "Sent message : " + msg; ;
+            Assert.AreEqual(expected, Prompt.DisplaySentMessage(msg));
+        }
+    }
+}
